e080d3cdd6a24db3d8903e81a11ef5953d47c961
ABD-UL-RAHMAN <99986811+ABD-UL-RAHMAN@users.noreply.github.com>
2023-12-06 14:58:55 +0500

    Update Chapter 5 - Case Study Hacker Statistics.py

--- Intermediate Python for Data Science/Chapter 5 - Case Study Hacker Statistics.py
+++ Intermediate Python for Data Science/Chapter 5 - Case Study Hacker Statistics.py
@@ -222,36 +222,40 @@ for x in range(100) :
 print(random_walk)
 
 #Implement clumsiness
-
 import matplotlib.pyplot as plt
 import numpy as np
+
 np.random.seed(123)
 all_walks = []
 
-# Simulate random walk 250 times
-for i in range(250) :
+# Simulate random walk 20 times
+for i in range(20):
     random_walk = [0]
-    for x in range(100) :
+    
+    for x in range(100):
         step = random_walk[-1]
-        dice = np.random.randint(1,7)
+        dice = np.random.randint(1, 7)
+        
         if dice <= 2:
             step = max(0, step - 1)
         elif dice <= 5:
             step = step + 1
         else:
-            step = step + np.random.randint(1,7)
+            step = step + np.random.randint(1, 7)
 
         # Implement clumsiness
-        if np.random.rand() <= 0.001 :
+        if np.random.rand() <= 0.005:
             step = 0
 
         random_walk.append(step)
+    
     all_walks.append(random_walk)
 
 # Create and plot np_aw_t
 np_aw_t = np.transpose(np.array(all_walks))
 plt.plot(np_aw_t)
 plt.show()
+
 
 #Plot the distribution
 import matplotlib.pyplot as plt
